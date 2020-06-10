678fee7977d8e9d4cdc04c74c9099e4b71312ff6
Satyam Kumar <satyamthedeveloper@gmail.com>
2020-06-10 09:20:58 +0530

    hands-on update 2

--- Day1/Hands-on-1/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Day1/Hands-on-1/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -8,26 +8,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
 		displayDate();
 	}
 	
 	public static void displayDate() {
+		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
 		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-		Date test = null;
+		Date date = null;
 		try {
-			test = format.parse("31/12/2018");
+			date = format.parse("31/12/2018");
+			
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(test);
+		System.out.println(date);
+		LOGGER.debug(date.toString());
+		LOGGER.info("END");
 	}
 
+	
 }
