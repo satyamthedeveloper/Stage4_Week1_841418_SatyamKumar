501c48c5553b2184a9be8652113436c4db53aac8
Satyam Kumar <satyamthedeveloper@gmail.com>
2020-06-12 20:38:23 +0530

    Hackerrank Day3 Codes

---
+++ Day1/Hands-on-1/spring-learn/src/main/java/com/cognizant/springlearn/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+	@RequestMapping(value="/hello")
+	public String sayHello() {
+		LOGGER.info("sayHello() START");
+		LOGGER.info("sayHello() ENDS");
+		return "Hello World!!";
+	}
+	
+	
+	
+}
---
+++ Day1/Hands-on-1/spring-learn/src/main/java/com/cognizant/springlearn/service/exception.java
@@ -0,0 +1,12 @@
+package com.cognizant.springlearn.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Country not found")
+public class exception extends Exception {
+	
+	public void CountryNotFoundException() {
+
+	}
+}
